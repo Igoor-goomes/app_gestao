f3ec3c204acfcad6daed5df2b387c51678600fff
Igor Gomes de Brto <igorgomesdebrito.dev@gmail.com>
2022-11-23 23:36:40 -0300

    Implantação dos switch case

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,10 +9,28 @@ class FornecedorController extends Controller
     public function index()
     {
         $fornecedores =[
-            0 => ['nome'=>'IGEPLAC', 'status'=>'Inativo'],
-            1 => ['nome'=>'UNIASSELVI', 'status'=>'Inativo'],
-            2 => ['nome'=>'UNIMAUA', 'status'=>'Inativo']
+            0 => [
+            'nome'=>'IGEPLAC',
+            'status'=>'Ativo',
+            'cnpj'=>'33.344.443/0001-20',
+            'ddd'=>'',
+            'telefone'=>'99572-1709'
+            ],
+            1 => [
+                'nome'=>'UEG',
+                'status'=>'Ativo',
+                'cnpj'=>'34.355.553/0001-30',
+                'ddd'=>'62',
+                'telefone'=>'99570-7070'
+            ],
+            2 => [
+                'nome'=>'FAFIBE',
+                'status'=>'Ativo',
+                'cnpj'=>'70.185.188/1001-41',
+                'ddd'=>'32',
+                'telefone'=>'99597-8558'
+            ],
         ];
-        return view('app.fornecedor.index', compact('fornecedores'));
+        return view('app.fornecedor.index',compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -4,19 +4,42 @@
 
 @php
     /*
-    if(<!condição>){} //enquanto executa se o retorno for true
+    if(empty($variavel)) {} //retorna trues se variavel estiver vazia
+    Uma variável pode se considerar vazia com os seguintes valores:
+    -''
+    -0
+    -0.0
+    -'0'
+    -null
+    -false
+    -array()
+    -$var
     */
 @endphp
-{{--@unless executa se o retorno for false--}}
+@isset($fornecedores)
+    Nome do Fornecedor: {{$fornecedores [0]['nome']}}
+    <br>
+    Status do Fornecedor: {{$fornecedores [0]['status']}}
+    <br>
+    CNPJ do Fornecedor: {{$fornecedores[0]['cnpj']}}
+    <br>
+    Telefone do Fornecedor: ({{$fornecedores[0]['ddd'] ?? ''}}) {{$fornecedores[0]['telefone'] ?? ''}}
+    @switch($fornecedores[0]['ddd'])
+        @case('61')
+            /Brasilia - DF
+            @break
+        @case('62')
+            /Goiania - GO
+            @break
+        @case('32')
+            /Boa Esperança - MG
+            @break
+        @default
+            Estado não identificado
+    @endswitch
+@endisset
 
-Nome do Fornecedor: {{$fornecedores [1]['nome']}}
-<br>
-Status do Fornecedor: {{$fornecedores [1]['status']}}
-<br>
-<br>
-@unless($fornecedores[1]['status']=='Ativo')
-    Fornecedor Inativado
-@endunless
+
 
 
 
