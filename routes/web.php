6585fa8f57727e50e67b67e1a80d2df3e3153976
Igor Gomes de Brto <igorgomesdebrito.dev@gmail.com>
2022-11-26 23:22:41 -0300

    finalização de template

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Rotas via crontroller
+
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre', 'SobreNosController@sobreNos')->name('site.sobre');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function (){return 'Login';})->name('site.login');
 
 
 //Grupo de rotas - app
+
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function (){return 'Clientes';})->name('app.clientes');
     Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
@@ -28,6 +31,7 @@ Route::prefix('/app')->group(function(){
 });
 
 //Rotas de contigência (fallback)
+
 Route::fallback(function () {
     echo 'URL acessada não existe, <a href="'.route('site.index').'">clique aqui</a> para ir para página principal'; 
 });
@@ -35,4 +39,5 @@ Route::fallback(function () {
 
 
 // Exemplos para testes
+
 Route::get('/teste/{parametro1}/{parametro2}','TesteController@teste')->name('teste');
